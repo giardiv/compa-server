0fd3ab368dbe88c5ada2d0f00cb8da0fce6028a5
Vincent <giardina.v@gmail.com>
2018-03-29 18:51:38 +0200

    morphia installed and running

--- src/main/compa/App/Container.java
+++ src/main/compa/App/Container.java
@@ -1,19 +1,23 @@
 package main.compa.App;
 
+import com.mongodb.MongoClient;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import main.compa.Controller.LocationController;
+import main.compa.Model.Location;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
 
 public class Container extends AbstractVerticle{
 
     private static Container INSTANCE = new Container();
 
     private Router router;
+    private Datastore datastore;
+    private ModelManager modelManager;
 
     private Container(){ }
 
@@ -22,7 +26,6 @@ public class Container extends AbstractVerticle{
     }
 
     public void run(){
-
         Container.getInstance().start();
     }
 
@@ -33,6 +36,14 @@ public class Container extends AbstractVerticle{
 
         server.requestHandler(router::accept).listen(8080);
         router.route().handler(BodyHandler.create());
+
+        final Morphia morphia = new Morphia();
+        morphia.mapPackage("main.compa.Model");
+        datastore = morphia.createDatastore(new MongoClient(), "compa");
+        datastore.ensureIndexes();
+
+        // Test adding
+        datastore.save(new Location());
 
         this.lunchController();
     }
@@ -45,5 +56,12 @@ public class Container extends AbstractVerticle{
         return this.router;
     }
 
+    public ModelManager getModelManager(){
+        return this.modelManager;
+    }
+
+    public Datastore getDataStore(){
+        return this.datastore;
+    }
     // TODO: Service manager
 }
